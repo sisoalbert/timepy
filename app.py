96216499209447b7347a6d56688a9f7942cd4f78
sisoalbert <sisoalbert@yahoo.com>
2023-02-09 00:32:39 +0200

    header change

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response
 from datetime import datetime
 
 app = Flask(__name__)
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route("/time")
 def get_current_time():
     current_time = str(datetime.now().time())
-    return current_time
+    response = make_response(current_time)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 if __name__ == '__main__':
     app.run()
